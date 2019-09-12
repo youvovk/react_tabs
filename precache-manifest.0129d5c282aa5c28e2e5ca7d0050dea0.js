5504157f782368eb47a10025deb8d07ee2be169c
youvovk <wer3wollf@gmail.com>
2019-09-12 11:22:03 +0300

    Updates

---
+++ precache-manifest.0129d5c282aa5c28e2e5ca7d0050dea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6343aacc351e1605c68a9102f16ccaa1",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "2f7f1bcf1568a722caca",
+    "url": "/react_tabs/static/css/main.8b01a05a.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "2f7f1bcf1568a722caca",
+    "url": "/react_tabs/static/js/main.481bb9d1.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
